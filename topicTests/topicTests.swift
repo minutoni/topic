b0951cbd736d1d45441e5f0feb89c4a20ea4de76
minutoni <minutoni105@gmail.com>
2020-06-26 22:32:55 +0900

    Initial Commit

---
+++ topicTests/topicTests.swift
@@ -0,0 +1,34 @@
+//
+//  topicTests.swift
+//  topicTests
+//
+//  Created by 所紀彦 on 2020/06/26.
+//  Copyright © 2020 所紀彦. All rights reserved.
+//
+
+import XCTest
+@testable import topic
+
+class topicTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
